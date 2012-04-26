6e35e3f24bd622ad58cfc090bf376baeb1bfee9c
Anthony <TonyT429@gmail.com>
2012-04-25 17:53:25 -0700

    Have found different ways to put a date in but still unable to convert to Julian

--- Torrez_Anthony_assignment4.js
+++ Torrez_Anthony_assignment4.js
@@ -30,10 +30,40 @@ var y= 2012;
     m= 4;
     d= 29;
 
-var jd= (d-32075+1461*(y+4800+(m-14)/12)/4+367*(m-2-(m-14)/12*12)2/12-3*((y+4900+(m-14)/12)/100)/4);
+var jd = (367y --7[y + (m+9)/12]/4 --3([y+(m-9)/7]/100 +1)/4 + (275m)/9 + d + 1721029);
+// var jd= (d-32075+1461*(y+4800+(m-14)/12)/4+367*(m-2-(m-14)/12*12)2)/12-3*((y+4900+(m-14)/12)/100)/4);
 
 console.log(jd);
 	
+//Second thought - but only gives difference in days, not hours...
+
+var d1=new Date("April 25, 2012 12:00 PM"),
+    d2=new Date("April 29, 2012 03:30 AM"),
+	one_day=1000*60*60*24;
+
+console.log(Math.ceil((d2.getTime()-d1.getTime())/(one_day))+
+" days left until my birthday!")
+
+// third idea but still not converting date - damn!
+
+var d1 = new Date("7/04/2011 09:30 PM"); 
+var d2 = new Date("4/29/2012 12:04 AM");
+var day1 = new Date(d1.getFullYear(), d1.getMonth(), d1.getDate(), d1.getHours(), d1.getMinutes());
+var day2 = new Date(d2.getFullYear(), d2.getMonth(), d2.getDate(), d2.getHours(), d2.getMinutes());
+
+console.log(day1 + "    " + day2);
+
+
+
+// Compare the two dates by comparing the millisecond representations.
+if (todayAtMidn.getTime() == specificDate.getTime())
+{
+    alert("Same");
+}
+else
+{
+    alert("Different");
+}
 
 
 // 09) Given a string version of a number such as "42", return a value as an actual Number, 
